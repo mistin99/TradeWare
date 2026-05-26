c903adb9a858cde64aa8dd179d6ecccea89c743f
Mastan Mastanov <mistin_@abv.bg>
2026-05-26 23:45:54 +0300

    fix(tradeware): Avoid creating a new engine each time and add a simple test to check connection

--- src/trade_ware/database/session.py
+++ src/trade_ware/database/session.py
@@ -7,21 +7,23 @@ from sqlalchemy.orm import Session, sessionmaker
 from trade_ware.core.config import settings
 
 
-def _get_session() -> Session:
-    engine_cls = create_engine(
-        settings.database_url,
-        isolation_level="REPEATABLE READ",
-        pool_pre_ping=True,
-        pool_recycle=3600,
-        connect_args={"max_allowed_packet": 512 * 1024 * 1024},
-    )
-    session_facotry = sessionmaker(autocommit=False, autoflush=False, bind=engine_cls)
-    return session_facotry()
+engine = create_engine(
+    settings.database_url,
+    isolation_level="REPEATABLE READ",
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
+
+SessionFactory = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 
 def get_db() -> Generator[Session, None, None]:
     """Get a database session."""
-    db = _get_session()
+    db = SessionFactory()
     try:
         yield db
     finally:
